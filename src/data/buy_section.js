c88a1401f0c67da1534c63e165e942c03c2a7201
fedeparrilla10 <federicoivanpd@gmail.com>
2023-10-11 22:56:07 +0300

    [change] refactoring

--- src/data/buy_section.js
+++ src/data/buy_section.js
@@ -1,9 +1,7 @@
-export const buySectionData = [
-  {
-    background: '/testingsecond/bgrig8002.png',
-    price: '199.90 €',
-  },
-];
+export const buySectionData = {
+  background: '/testingsecond/bgrig8002.png',
+  price: '199.90 €',
+};
 
 export const buySectionStores = [
   {
@@ -20,5 +18,28 @@ export const buySectionStores = [
     id: 3,
     name: 'Fnac',
     src: '/testingsecond/fnac.png',
+  },
+];
+
+export const productData = [
+  {
+    imgSrc: '/testingsecond/800prohdbox.webp',
+    title: '800 PRO HD',
+    logoSrc: '/testingsecond/pc.svg',
+    class: 'w-10 h-10',
+  },
+  {
+    imgSrc: '/testingsecond/800prohsbox.webp',
+    title: '800 PRO HS',
+    logoSrc: '/testingsecond/ps4.svg',
+    secondLogoSrc: '/testingsecond/ps5.svg',
+    altText: 'PS4 & PS5',
+    class: 'w-14 h-14',
+  },
+  {
+    imgSrc: '/testingsecond/800prohxbox.png',
+    title: '800H PRO HX',
+    logoSrc: '/testingsecond/xbox.svg',
+    class: 'w-14 h-14',
   },
 ];
--- src/data/first_section.js
+++ src/data/first_section.js
@@ -4,17 +4,21 @@ export const firstSectionData = [
     img: '/testingsecond/pc1.jpg',
     title: '800 PRO HD',
     icon: '/testingsecond/pc.svg',
+    class: 'w-16 h-16 pl-5',
   },
   {
     id: 2,
     img: '/testingsecond/ps5.jpg',
     title: '800 PRO HS',
-    icon: '/testingsecond/ps5.svg',
+    icon: '/testingsecond/ps4.svg',
+    extraIcon: '/testingsecond/ps5.svg',
+    class: 'w-16 h-16',
   },
   {
     id: 3,
     img: '/testingsecond/xbox.jpg',
     title: '800 PRO HX',
     icon: '/testingsecond/xbox.svg',
+    class: 'w-16 h-16',
   },
 ];
