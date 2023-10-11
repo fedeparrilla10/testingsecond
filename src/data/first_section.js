c3f4d4fffcff56675e873f4ec1ec3b7f7c1c5fcc
fedeparrilla10 <federicoivanpd@gmail.com>
2023-10-11 22:08:07 +0300

    [change] refactoring

--- src/data/first_section.js
+++ src/data/first_section.js
@@ -1,9 +1,20 @@
 export const firstSectionData = [
   {
-    title: 'Comodidad y rendimiento extremo en...',
-    firstImg: {
-      src: '/testingsecond/rig800frontal.png',
-      alt: 'RIG 800 PRO',
-    },
+    id: 1,
+    img: '/testingsecond/pc1.jpg',
+    title: '800 PRO HD',
+    icon: '/testingsecond/pc.svg',
+  },
+  {
+    id: 2,
+    img: '/testingsecond/ps5.jpg',
+    title: '800 PRO HS',
+    icon: '/testingsecond/ps5.svg',
+  },
+  {
+    id: 3,
+    img: '/testingsecond/xbox.jpg',
+    title: '800 PRO HX',
+    icon: '/testingsecond/xbox.svg',
   },
 ];
