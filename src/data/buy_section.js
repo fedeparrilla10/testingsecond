cb6dde2a0824fa358745ac4d15d006affbef8434
fedeparrilla10 <federicoivanpd@gmail.com>
2023-11-06 21:10:07 +0200

    [update] proper links at buying section

--- src/data/buy_section.js
+++ src/data/buy_section.js
@@ -3,30 +3,32 @@ export const buySectionData = {
   price: '199.90 €',
 };
 
-export const buySectionStores = [
-  {
-    id: 1,
-    name: 'Game',
-    src: '/testingsecond/game.png',
-  },
-  {
-    id: 2,
-    name: 'El Corte Inglés',
-    src: '/testingsecond/corteinglés.png',
-  },
-  {
-    id: 3,
-    name: 'Fnac',
-    src: '/testingsecond/fnac.png',
-  },
-];
-
 export const productData = [
   {
     imgSrc: '/testingsecond/800prohdbox.webp',
     title: '800 PRO HD',
     logoSrc: '/testingsecond/pc.svg',
     class: 'w-10 h-10',
+    buySectionStores: [
+      {
+        id: 1,
+        name: 'Game',
+        src: '/testingsecond/game.png',
+        link: 'https://www.game.es/auriculares-gaming-rig-serie-800-pro-hd-multiplataforma-219108',
+      },
+      {
+        id: 2,
+        name: 'MediaMarkt',
+        src: '/testingsecond/mediamarkt.png',
+        link: 'https://www.mediamarkt.es/es/product/_auriculares-gaming-nacon-rig-800-pro-hd-inalambrico-24h-autonomia-para-pc-negro-1557284.html',
+      },
+      {
+        id: 3,
+        name: 'Fnac',
+        src: '/testingsecond/fnac.png',
+        link: 'https://www.fnac.es/Headset-gaming-Nacon-RIG-800-Pro-HD-Negro-Accesorio-Videoconsola-Auriculares-para-consola/a10281839#omnsearchpos=1',
+      },
+    ],
   },
   {
     imgSrc: '/testingsecond/800prohsbox.webp',
@@ -35,11 +37,63 @@ export const productData = [
     secondLogoSrc: '/testingsecond/ps5.svg',
     altText: 'PS4 & PS5',
     class: 'w-14 h-14',
+    buySectionStores: [
+      {
+        id: 1,
+        name: 'Game',
+        src: '/testingsecond/game.png',
+        link: 'https://www.game.es/auriculares-gaming-rig-serie-800-pro-hs-negro-playstation-4-207041',
+      },
+      {
+        id: 2,
+        name: 'Carrefour',
+        src: '/testingsecond/carrefour.png',
+        link: 'https://www.carrefour.es/auriculares-gaming-nacon-rig-800-pro-hs-para-ps5-y-ps4/VC4A-20870742/p',
+      },
+      {
+        id: 3,
+        name: 'Fnac',
+        src: '/testingsecond/fnac.png',
+        link: 'https://www.fnac.es/Headset-gaming-RIG-800-PRO-HS-Negro-Accesorio-Videoconsola-Auriculares-para-consola/a9808541#omnsearchpos=1',
+      },
+      {
+        id: 4,
+        name: 'MediaMarkt',
+        src: '/testingsecond/mediamarkt.png',
+        link: 'https://www.mediamarkt.es/es/product/_auriculares-gaming-nacon-rig-800-pro-hs-inalambrico-24h-autonomia-para-ps4-y-ps5-negro-1557282.html',
+      },
+    ],
   },
   {
     imgSrc: '/testingsecond/800prohxbox.png',
     title: '800H PRO HX',
     logoSrc: '/testingsecond/xbox.svg',
     class: 'w-14 h-14',
+    buySectionStores: [
+      {
+        id: 1,
+        name: 'Game',
+        src: '/testingsecond/game.png',
+        link: 'https://www.game.es/auriculares-gaming-rig-serie-800-pro-hx-xbox-one-211677',
+      },
+      {
+        id: 2,
+        name: 'Carrefour',
+        src: '/testingsecond/carrefour.png',
+        link: 'https://www.carrefour.es/auriculares-gaming-nacon-rig-serie-800-pro-hx-para-xbox/VC4A-20870701/p',
+      },
+      {
+        id: 3,
+        name: 'Fnac',
+        src: '/testingsecond/fnac.png',
+        link: 'https://www.fnac.es/Headset-gaming-RIG-800-PRO-HX-Negro-Accesorio-Videoconsola-Auriculares-para-consola/a9808540#omnsearchpos=2',
+      },
+      {
+        id: 4,
+        name: 'MediaMarkt',
+        src: '/testingsecond/mediamarkt.png',
+        link: 'https://www.mediamarkt.es/es/product/_auriculares-gaming-nacon-rig-800-pro-hx-inalambrico-24h-autonomia-para-xbox-negro-1557283.html',
+      },
+    ],
   },
 ];
