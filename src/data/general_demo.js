42620ffcfe37c86839591bad8f6ded9b4640b5ad
fedeparrilla10 <federicoivanpd@gmail.com>
2023-11-07 18:04:09 +0200

    [add] new videos + favicon

---
+++ src/data/general_demo.js
@@ -0,0 +1,49 @@
+export const firstGeneralDemoData = [
+  {
+    id: 1,
+    video: '/testingsecond/videos_isidro/PC_DONGLE.mp4',
+    title: '800 PRO HD',
+    icon: '/testingsecond/blackpc.svg',
+    class: 'w-16 h-16 pl-5',
+  },
+  {
+    id: 2,
+    video: '/testingsecond/videos_isidro/PS5_DONGLE.mp4',
+    title: '800 PRO HS',
+    icon: '/testingsecond/blackps4.svg',
+    extraIcon: '/testingsecond/blackps5.svg',
+    class: 'w-16 h-16',
+  },
+  {
+    id: 3,
+    video: '/testingsecond/videos_isidro/XBOX_DONGLE.mp4',
+    title: '800 PRO HX',
+    icon: '/testingsecond/xbox.svg',
+    class: 'w-16 h-16',
+  },
+];
+
+export const secondGeneralDemoData = [
+  {
+    id: 1,
+    video: '/testingsecond/videos_isidro/PC_USB.mp4',
+    title: '800 PRO HD',
+    icon: '/testingsecond/pc.svg',
+    class: 'w-16 h-16 pl-5',
+  },
+  {
+    id: 2,
+    video: '/testingsecond/videos_isidro/PS5_USB.mp4',
+    title: '800 PRO HS',
+    icon: '/testingsecond/ps4.svg',
+    extraIcon: '/testingsecond/ps5.svg',
+    class: 'w-16 h-16',
+  },
+  {
+    id: 3,
+    video: '/testingsecond/videos_isidro/XBOX_USB.mp4',
+    title: '800 PRO HX',
+    icon: '/testingsecond/xbox.svg',
+    class: 'w-16 h-16',
+  },
+];
